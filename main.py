48988f3d9fcb725d8c6f63f56f111f6ce22fe96b
Preisler25 <preisler.andras@gmail.com>
2023-01-18 18:29:55 +0100

    main

--- main.py
+++ main.py
@@ -71,13 +71,60 @@ def feladat11(list: list):
     for i in list:
         print(i)
     for i in range(10):
-        print("Hello World!")
+        print("feladat11")
     for i in range(len(list)):
         print(list[i])
 def feladat12(num: int):
     while num < 10:
-        print("Hello World!")
+        print("feladat12")
         num += 1
+def feladat13(num: int):
+    if num < 18:
+        print('szám kisebb mint 18')
+    elif num >= 18 and num <= 30:
+        print ('a szám 18 és 30között van')
+    elif num >30  and num< 65:
+        print('a szám 30nál nagyobb de 65nél kisebb')
+    else:
+        print('szám 65nél nagyobb')
+def feladat14(num: int, num2: int, num3: int):
+    while num < 10:
+        num += 1
+        if num == 5:
+            break
+        print(f"feladat14 {num}")
+    while num2 < 10:
+        num2 += 1
+        if num2 == 5:
+            continue
+        print(f"feladat14 {num2}")
+    while num3 < 10:
+        num3 += 1
+        if num3 == 5:
+            pass
+        print(f"feladat14 {num3}")
+def feladat15(jelszo: str, proba: int):
+    user_input = input("Írj be egy jelszót: ")
+    while user_input != jelszo and proba > 0:
+        proba -= 1
+        user_input = input("Elrontotta a jelszót, kérjük probálja újra: ")
+    if proba == 0:
+        print("Túl sok próbálkozás!")
+    else:
+        print("Sikeres bejelentkezés!")
+def feladat16(list: list):
+    for i in list:
+        if type(i) == str:
+            print(i.upper())
+        else:
+            print(type(i))
+def feladat17(*args):
+    print(sum(args))
+def feladat18(n: float):
+    num = int(round(n, 0))
+    print(hex(num))
+    print(bin(num))
+    print(oct(num))
 #endregion
 #region Main
 def main():
@@ -93,5 +140,11 @@ def main():
     feladat10([1])
     feladat11([1, 2, 3])
     feladat12(2)
+    feladat13(2)
+    feladat14(2, 2, 2)
+    feladat15("alma", 3)
+    feladat16([1, "alma", 2, "korte", 3])
+    feladat17(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    feladat18(1.5)
 main()
 #endregion
